41cb799a18bde8f3157239d466db0e8b2de4a9eb
Duncan Seif <duncan.seif@hotmail.com>
2017-07-31 21:13:41 -0500

    Random changes to slider 4

--- slides/slide4.php
+++ slides/slide4.php
@@ -1,5 +1,8 @@
 <div id="slide4">
-    
     <h2 class="text-center" >Education</h2>
-    <p>Donec quis consectetur odio, eget condimentum sapien. Praesent facilisis porta mi quis luctus. Curabitur sed ullamcorper tortor. Donec et sapien orci. Aenean laoreet ante id nibh convallis dapibus. Vestibulum vitae efficitur est. Suspendisse laoreet, ipsum non commodo fermentum, nibh eros laoreet tellus, ut suscipit massa dolor non nulla. Phasellus lacus lacus, rutrum id blandit ullamcorper, pulvinar nec arcu. Donec eu orci faucibus, imperdiet tortor vitae, vulputate augue. Ut aliquam nulla eu lacus maximus sagittis. Vivamus vestibulum ultricies fermentum. Nunc maximus justo vel laoreet facilisis.</p>
+    <div class="row">
+        <div class="col-6">
+            <p>Donec quis consectetur odio, eget condimentum sapien. Praesent facilisis porta mi quis luctus. Curabitur sed ullamcorper tortor. Donec et sapien orci. Aenean laoreet ante id nibh convallis dapibus. Vestibulum vitae efficitur est. Suspendisse laoreet, ipsum non commodo fermentum, nibh eros laoreet tellus, ut suscipit massa dolor non nulla. Phasellus lacus lacus, rutrum id blandit ullamcorper, pulvinar nec arcu. Donec eu orci faucibus, imperdiet tortor vitae, vulputate augue. Ut aliquam nulla eu lacus maximus sagittis. Vivamus vestibulum ultricies fermentum. Nunc maximus justo vel laoreet facilisis.</p>
+        </div>
+    </div>
 </div>
